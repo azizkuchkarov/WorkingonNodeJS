b96b57cbcc28bf481e302a4e6ab632d9e54a87ad
azizkuchkarov <aziz_kuchkarov91@mail.ru>
2023-04-28 13:59:42 +0500

    working on NodeJS

---
+++ car.js
@@ -0,0 +1,6 @@
+const car = { name: "mers", color: "black" };
+const carLogger = () => {
+  console.log("Logger car");
+};
+
+module.exports = { car, carLogger };
---
+++ user.js
@@ -0,0 +1,6 @@
+const user = { name: "Aziz" };
+const userLogger = () => {
+  console.log("Logger user");
+};
+
+module.exports = { user, userLogger };
